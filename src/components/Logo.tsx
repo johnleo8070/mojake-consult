cd5a830db62c4e8a76543bcd86e1cd1e6c35bda6
John leo <johntoluwalase80@gmail.com>
2026-04-09 12:59:24 +0100

    Fix logo filenames and paths

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -10,19 +10,19 @@ export default function Logo({ className = '', variant = 'navbar' }: LogoProps) 
 
   switch (variant) {
     case 'navbar':
-      src = '/mojake-consult-logo 3.png';
+      src = '/mojake-consult-logo-3.png';
       height = '48px';
       break;
     case 'footer':
-      src = '/mojake-consult-logo 2.png';
+      src = '/mojake-consult-logo-2.png';
       height = '56px';
       break;
     case 'generic':
-      src = '/mojake-consult-logo 1.png';
+      src = '/mojake-consult-logo-1.png';
       height = '48px';
       break;
     default:
-      src = '/mojake-consult-logo 3.png';
+      src = '/mojake-consult-logo-3.png';
   }
 
   return (
